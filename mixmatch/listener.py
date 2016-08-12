7017a072e09b286a07976e740dfa9992e7777e2b
George Silvis, III <george.iii.silvis@gmail.com>
2016-08-12 11:44:28 -0400

    Listen for snapshots and images too

--- mixmatch/listener.py
+++ mixmatch/listener.py
@@ -15,7 +15,6 @@ class VolumeCreateEndpoint(object):
     filter_rule = oslo_messaging.NotificationFilter(
             publisher_id='^volume.*',
             event_type='^volume.create.start$')
-
     def info(self, ctxt, publisher_id, event_type, payload, metadata):
         LOG.info('Creating volume mapping %s -> %s at %s' % (
                 payload['volume_id'],
@@ -26,14 +25,12 @@ class VolumeCreateEndpoint(object):
                 payload['tenant_id'],
                 self.sp_name))
 
-
 class VolumeDeleteEndpoint(object):
     def __init__(self, sp_name):
         self.sp_name = sp_name
     filter_rule = oslo_messaging.NotificationFilter(
             publisher_id='^volume.*',
             event_type='^volume.delete.end$')
-
     def info(self, ctxt, publisher_id, event_type, payload, metadata):
         LOG.info('Deleting volume mapping %s -> %s at %s' % (
                 payload['volume_id'],
@@ -41,10 +38,75 @@ class VolumeDeleteEndpoint(object):
                 self.sp_name))
         delete(ResourceMapping.find("volume", payload['volume_id']))
 
+class SnapshotCreateEndpoint(object):
+    def __init__(self, sp_name):
+        self.sp_name = sp_name
+    filter_rule = oslo_messaging.NotificationFilter(
+            publisher_id='^snapshot.*',
+            event_type='^snapshot.create.start$')
+    def info(self, ctxt, publisher_id, event_type, payload, metadata):
+        LOG.info('Creating snapshot mapping %s -> %s at %s' % (
+                payload['snapshot_id'],
+                payload['tenant_id'],
+                self.sp_name))
+        insert(ResourceMapping("snapshot",
+                payload['snapshot_id'],
+                payload['tenant_id'],
+                self.sp_name))
+
+class SnapshotDeleteEndpoint(object):
+    def __init__(self, sp_name):
+        self.sp_name = sp_name
+    filter_rule = oslo_messaging.NotificationFilter(
+            publisher_id='^snapshot.*',
+            event_type='^snapshot.delete.end$')
+    def info(self, ctxt, publisher_id, event_type, payload, metadata):
+        LOG.info('Deleting snapshot mapping %s -> %s at %s' % (
+                payload['snapshot_id'],
+                payload['tenant_id'],
+                self.sp_name))
+        delete(ResourceMapping.find("snapshot", payload['snapshot_id']))
+
+class ImageCreateEndpoint(object):
+    def __init__(self, sp_name):
+        self.sp_name = sp_name
+    filter_rule = oslo_messaging.NotificationFilter(
+            publisher_id='^image.*',
+            event_type='^image.create$')
+    def info(self, ctxt, publisher_id, event_type, payload, metadata):
+        LOG.info('Creating image mapping %s -> %s at %s' % (
+                payload['id'],
+                payload['owner'],
+                self.sp_name))
+        insert(ResourceMapping("image",
+                payload['id'],
+                payload['owner'],
+                self.sp_name))
+
+class ImageDeleteEndpoint(object):
+    def __init__(self, sp_name):
+        self.sp_name = sp_name
+    filter_rule = oslo_messaging.NotificationFilter(
+            publisher_id='^image.*',
+            event_type='^image.delete$')
+    def info(self, ctxt, publisher_id, event_type, payload, metadata):
+        LOG.info('Deleting image mapping %s -> %s at %s' % (
+                payload['id'],
+                payload['owner'],
+                self.sp_name))
+        delete(ResourceMapping.find("image", payload['id']))
+
 
 def get_server_for_sp(sp):
     cfg = CONF.__getattr__('sp_%s' % sp)
-    endpoints = [VolumeCreateEndpoint(cfg.sp_name), VolumeDeleteEndpoint(cfg.sp_name)]
+    endpoints = [
+            VolumeCreateEndpoint(cfg.sp_name),
+            VolumeDeleteEndpoint(cfg.sp_name),
+            SnapshotCreateEndpoint(cfg.sp_name),
+            SnapshotDeleteEndpoint(cfg.sp_name),
+            ImageCreateEndpoint(cfg.sp_name),
+            ImageDeleteEndpoint(cfg.sp_name)
+    ]
     transport = oslo_messaging.get_notification_transport(CONF, cfg.messagebus)
     targets = [oslo_messaging.Target(topic='notifications')]
     return oslo_messaging.get_notification_listener(transport, targets, endpoints, executor='eventlet')
